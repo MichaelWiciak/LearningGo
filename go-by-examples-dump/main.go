6de5db675815fa04bc684076c40315736162de98
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-05-24 11:14:43 +0100

    feat: add func type so can call funcs from a slice

---
+++ go-by-examples-dump/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Function struct {
+	Name string
+	Run func()
+}
+
+func if_statements() {
+	if 7%2 == 0 {
+        fmt.Println("7 is even")
+    } else {
+        fmt.Println("7 is odd")
+    }
+
+    if 8%4 == 0 {
+        fmt.Println("8 is divisible by 4")
+    }
+
+    if 8%2 == 0 || 7%2 == 0 {
+        fmt.Println("either 8 or 7 are even")
+    }
+
+    if num := 9; num < 0 {
+        fmt.Println(num, "is negative")
+    } else if num < 10 {
+        fmt.Println(num, "has 1 digit")
+    } else {
+        fmt.Println(num, "has multiple digits")
+    }
+}
+
+func for_statements() {
+	    i := 1
+    for i <= 3 {
+        fmt.Println(i)
+        i = i + 1
+    }
+
+    for j := 0; j < 3; j++ {
+        fmt.Println(j)
+    }
+
+    for i := range 3 {
+        fmt.Println("range", i)
+    }
+
+    for {
+        fmt.Println("loop")
+        break
+    }
+
+    for n := range 6 {
+        if n%2 == 0 {
+            continue
+        }
+        fmt.Println(n)
+    }
+}
+
+func func_separator() {
+	fmt.Println("---------")
+}
+
+func switch_statements() {
+
+    i := 2
+    fmt.Print("Write ", i, " as ")
+    switch i {
+    case 1:
+        fmt.Println("one")
+    case 2:
+        fmt.Println("two")
+    case 3:
+        fmt.Println("three")
+    }
+
+    switch time.Now().Weekday() {
+    case time.Saturday, time.Sunday:
+        fmt.Println("It's the weekend")
+    default:
+        fmt.Println("It's a weekday")
+    }
+
+    t := time.Now()
+    switch {
+    case t.Hour() < 12:
+        fmt.Println("It's before noon")
+    default:
+        fmt.Println("It's after noon")
+    }
+
+    whatAmI := func(i interface{}) {
+        switch t := i.(type) {
+        case bool:
+            fmt.Println("I'm a bool")
+        case int:
+            fmt.Println("I'm an int")
+        default:
+            fmt.Printf("Don't know type %T\n", t)
+        }
+    }
+    whatAmI(true)
+    whatAmI(1)
+    whatAmI("hey")
+	
+}
+
+func main() {
+
+	functions := []Function{
+		{"if statements", if_statements},
+		{"for statements", for_statements},
+		{"switch statements", switch_statements},
+	}
+
+	for _, f := range functions {
+		fmt.Println(">>> Running:", f.Name)
+		f.Run()
+		func_separator()
+	}
+
+}
