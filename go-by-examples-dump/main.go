e381afb9810b909964b9c093a52e883b72303be6
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-06-17 08:31:01 +0100

    feat: add maps

--- go-by-examples-dump/main.go
+++ go-by-examples-dump/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
     "slices"
+    "maps"
 )
 
 type Function struct {
@@ -198,6 +199,42 @@ func slices_example() {
     fmt.Println("2d: ", twoD)
 }
 
+func maps_examples() {
+
+    m := make(map[string]int)
+
+    m["k1"] = 7
+    m["k2"] = 13
+
+    fmt.Println("map:", m)
+
+    v1 := m["k1"]
+    fmt.Println("v1:", v1)
+
+    v3 := m["k3"]
+    fmt.Println("v3:", v3)
+
+    fmt.Println("len:", len(m))
+
+    delete(m, "k2")
+    fmt.Println("map:", m)
+
+    clear(m)
+    fmt.Println("map:", m)
+
+    _, prs := m["k2"]
+    fmt.Println("prs:", prs)
+
+    n := map[string]int{"foo": 1, "bar": 2}
+    fmt.Println("map:", n)
+
+    n2 := map[string]int{"foo": 1, "bar": 2}
+    if maps.Equal(n, n2) {
+        fmt.Println("n == n2")
+    }
+}
+
+
 func main() {
 
 	functions := []Function{
@@ -206,6 +243,7 @@ func main() {
 		{"switch statements", switch_statements},
 		{"arrays", arrays},
         {"slices", slices_example},
+        {"maps", maps_examples},
 	}
 
 	for _, f := range functions {
