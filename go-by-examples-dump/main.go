38f9431f286758171d4d2c2e29bde8fa92028fb8
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-06-24 16:57:08 +0100

    feat: add recursive functions

--- go-by-examples-dump/main.go
+++ go-by-examples-dump/main.go
@@ -278,6 +278,29 @@ func closures_lambdas() {
     fmt.Println(newInts())
 }
 
+func fact(n int) int {
+    if n == 0 {
+        return 1
+    }
+    return n * fact(n-1)
+}
+
+func recursive_functions() {
+    fmt.Println(fact(7))
+
+    var fib func(n int) int
+
+    fib = func(n int) int {
+        if n < 2 {
+            return n
+        }
+
+        return fib(n-1) + fib(n-2)
+    }
+
+    fmt.Println(fib(7))
+}
+
 func main() {
 
 	functions := []Function{
@@ -289,6 +312,7 @@ func main() {
 		{"maps", maps_examples},
 		{"variadic function", variadic_function_caller},
 		{"closures and lambdas", closures_lambdas},
+		{"recursive functions", recursive_functions},
 	}
 
 	for _, f := range functions {
