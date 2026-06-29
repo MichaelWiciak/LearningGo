cee62b03f692f25b060ddeb402048a1fc00afb6e
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-06-29 17:08:56 +0100

    feat: add generics

--- go-by-examples-dump/main.go
+++ go-by-examples-dump/main.go
@@ -582,6 +582,56 @@ func embedded_structs() {
 	fmt.Println("describer:", d.describe())
 }
 
+func SlicesIndex[S ~[]E, E comparable](s S, v E) int {
+	for i := range s {
+		if v == s[i] {
+			return i
+		}
+	}
+	return -1
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) AllElements() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
+}
+
+func generics() {
+	var s = []string{"foo", "bar", "zoo"}
+
+	fmt.Println("index of zoo:", SlicesIndex(s, "zoo"))
+
+	_ = SlicesIndex[[]string, string](s, "zoo")
+
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+	fmt.Println("list:", lst.AllElements())
+}
+
 func main() {
 
 	functions := []Function{
@@ -602,6 +652,7 @@ func main() {
 		{"interfaces", interfaces},
 		{"enums", enums},
 		{"embedded_structs", embedded_structs},
+		{"generics", generics},
 	}
 
 	for _, f := range functions {
