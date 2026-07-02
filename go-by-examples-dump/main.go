182e46ddd110fce2c0bbf93dd4d3bcc25b2b7f87
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-07-02 22:00:19 +0100

    feat: add errors

--- go-by-examples-dump/main.go
+++ go-by-examples-dump/main.go
@@ -9,6 +9,7 @@ import (
 	stdstrings "strings"
 	"time"
 	"unicode/utf8"
+	"errors"
 )
 
 type T struct {
@@ -684,6 +685,56 @@ func range_over_iterator() {
 	}
 }
 
+
+func f(arg int) (int, error) {
+    if arg == 42 {
+
+        return -1, errors.New("can't work with 42")
+    }
+
+    return arg + 3, nil
+}
+
+var ErrOutOfTea = errors.New("no more tea available")
+var ErrPower = errors.New("can't boil water")
+
+func makeTea(arg int) error {
+    if arg == 2 {
+        return ErrOutOfTea
+    } else if arg == 4 {
+
+        return fmt.Errorf("making tea: %w", ErrPower)
+    }
+    return nil
+}
+
+func errors_example() {
+    for _, i := range []int{7, 42} {
+
+        if r, e := f(i); e != nil {
+            fmt.Println("f failed:", e)
+        } else {
+            fmt.Println("f worked:", r)
+        }
+    }
+
+    for i := range 5 {
+        if err := makeTea(i); err != nil {
+
+            if errors.Is(err, ErrOutOfTea) {
+                fmt.Println("We should buy new tea!")
+            } else if errors.Is(err, ErrPower) {
+                fmt.Println("Now it is dark.")
+            } else {
+                fmt.Printf("unknown error: %s\n", err)
+            }
+            continue
+        }
+
+        fmt.Println("Tea is ready!")
+    }
+}
+
 func main() {
 
 	functions := []Function{
@@ -706,6 +757,7 @@ func main() {
 		{"embedded_structs", embedded_structs},
 		{"generics", generics},
 		{"range_over_iterator", range_over_iterator},
+		{"errors", errors_example},
 	}
 
 	for _, f := range functions {
