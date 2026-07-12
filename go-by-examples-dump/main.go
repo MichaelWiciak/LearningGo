d56a201893ceeca2052ebae841e9b6d0dcb24d85
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-07-12 17:05:19 +0100

    feat: add case select statements

--- go-by-examples-dump/main.go
+++ go-by-examples-dump/main.go
@@ -836,6 +836,30 @@ func channel_directions() {
 	fmt.Println(<-pongs)
 }
 
+func select_statement() {
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+
+	for range 2 {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+}
+
 func main() {
 
 	functions := []Function{
@@ -865,6 +889,7 @@ func main() {
 		{"buffered channels", buffered_channels},
 		{"channel synchronisation", channel_synchronisation},
 		{"channel_directions", channel_directions},
+		{"select", select_statement},
 	}
 
 	for _, f := range functions {
