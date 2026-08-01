5379b6eb5900c2543ba95d10278c328fa94fc643
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-08-01 08:32:42 +0100

    feat: add time

--- go-by-examples-dump/main.go
+++ go-by-examples-dump/main.go
@@ -1607,6 +1607,43 @@ func xml_example() {
 	fmt.Println(string(out))
 }
 
+func time_example() {
+	p := fmt.Println
+
+	now := time.Now()
+	p(now)
+
+	then := time.Date(
+		2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+	p(then)
+
+	p(then.Year())
+	p(then.Month())
+	p(then.Day())
+	p(then.Hour())
+	p(then.Minute())
+	p(then.Second())
+	p(then.Nanosecond())
+	p(then.Location())
+
+	p(then.Weekday())
+
+	p(then.Before(now))
+	p(then.After(now))
+	p(then.Equal(now))
+
+	diff := now.Sub(then)
+	p(diff)
+
+	p(diff.Hours())
+	p(diff.Minutes())
+	p(diff.Seconds())
+	p(diff.Nanoseconds())
+
+	p(then.Add(diff))
+	p(then.Add(-diff))
+}
+
 func main() {
 
 	functions := []Function{
@@ -1660,6 +1697,7 @@ func main() {
 		{"regular expressions", regular_expessions},
 		{"json", json_example},
 		{"xml", xml_example},
+		{"time", time_example},
 	}
 
 	for _, f := range functions {
