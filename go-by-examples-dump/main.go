dfbb1e3f75d6c2f1523874dbfcce19287aef96f0
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-08-01 08:10:27 +0100

    feat: add json section

--- go-by-examples-dump/main.go
+++ go-by-examples-dump/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"cmp"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"iter"
@@ -1487,6 +1488,82 @@ func regular_expessions() {
 	fmt.Println(string(out))
 }
 
+type response1 struct {
+	Page   int
+	Fruits []string
+}
+
+type response2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruits"`
+}
+
+func json_example() {
+
+	bolB, _ := json.Marshal(true)
+	fmt.Println(string(bolB))
+
+	intB, _ := json.Marshal(1)
+	fmt.Println(string(intB))
+
+	fltB, _ := json.Marshal(2.34)
+	fmt.Println(string(fltB))
+
+	strB, _ := json.Marshal("gopher")
+	fmt.Println(string(strB))
+
+	slcD := []string{"apple", "peach", "pear"}
+	slcB, _ := json.Marshal(slcD)
+	fmt.Println(string(slcB))
+
+	mapD := map[string]int{"apple": 5, "lettuce": 7}
+	mapB, _ := json.Marshal(mapD)
+	fmt.Println(string(mapB))
+
+	res1D := &response1{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res1B, _ := json.Marshal(res1D)
+	fmt.Println(string(res1B))
+
+	res2D := &response2{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res2B, _ := json.Marshal(res2D)
+	fmt.Println(string(res2B))
+
+	byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
+
+	var dat map[string]any
+
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
+	}
+	fmt.Println(dat)
+
+	num := dat["num"].(float64)
+	fmt.Println(num)
+
+	strs := dat["strs"].([]any)
+	str1 := strs[0].(string)
+	fmt.Println(str1)
+
+	str := `{"page": 1, "fruits": ["apple", "peach"]}`
+	res := response2{}
+	_ = json.Unmarshal([]byte(str), &res)
+	fmt.Println(res)
+	fmt.Println(res.Fruits[0])
+
+	enc := json.NewEncoder(os.Stdout)
+	d := map[string]int{"apple": 5, "lettuce": 7}
+	_ = enc.Encode(d)
+
+	dec := json.NewDecoder(stdstrings.NewReader(str))
+	res1 := response2{}
+	_ = dec.Decode(&res1)
+	fmt.Println(res1)
+}
+
 func main() {
 
 	functions := []Function{
@@ -1538,6 +1615,7 @@ func main() {
 		{"string formatting", string_formatting},
 		{"text templates", text_templates},
 		{"regular expressions", regular_expessions},
+		{"json", json_example},
 	}
 
 	for _, f := range functions {
