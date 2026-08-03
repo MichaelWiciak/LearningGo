1291ae5e8f56afb02aca7a600f73e20caa806fea
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-08-03 15:23:50 +0100

    feat: add filepaths

--- go-by-examples-dump/main.go
+++ go-by-examples-dump/main.go
@@ -1897,6 +1897,40 @@ func line_filters() {
 	}
 }
 
+func filepaths() {
+
+	p := filepath.Join("dir1", "dir2", "filename")
+	fmt.Println("p:", p)
+
+	fmt.Println(filepath.Join("dir1//", "filename"))
+	fmt.Println(filepath.Join("dir1/../dir1", "filename"))
+
+	fmt.Println("Dir(p):", filepath.Dir(p))
+	fmt.Println("Base(p):", filepath.Base(p))
+
+	fmt.Println(filepath.IsAbs("dir/file"))
+	fmt.Println(filepath.IsAbs("/dir/file"))
+
+	filename := "config.json"
+
+	ext := filepath.Ext(filename)
+	fmt.Println(ext)
+
+	fmt.Println(stdstrings.TrimSuffix(filename, ext))
+
+	rel, err := filepath.Rel("a/b", "a/b/t/file")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rel)
+
+	rel, err = filepath.Rel("a/b", "a/c/t/file")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rel)
+}
+
 func main() {
 
 	functions := []Function{
@@ -1961,6 +1995,7 @@ func main() {
 		{"Reading Files", reading_files},
 		{"Writing Files", writing_example},
 		{"Line Filters", line_filters},
+		{"File paths", filepaths},
 	}
 
 	for _, f := range functions {
